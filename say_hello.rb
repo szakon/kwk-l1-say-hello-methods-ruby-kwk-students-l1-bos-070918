5c5fa4f4428f8265241676880e03df410bd3890b
szakon <sarahzakon@gmail.com>
2018-07-10 14:58:56 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,5 +1,5 @@
 # Build your say_hello method here
-def say_hello (your_name),(my_name)
+def say_hello (your_name,my_name)
   puts "Hello #{your_name}! I'm #{my_name}"
 end
 say_hello("Neath"),("Sarah")
